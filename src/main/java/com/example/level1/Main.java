d17636b6ffb7e26f20f74df1bba70bf31068f3fd
minkyungCHO <mk981015@naver.com>
2025-01-13 15:39:20 +0900

    feat: 문자 입력 예외 처리

--- src/main/java/com/example/level1/Main.java
+++ src/main/java/com/example/level1/Main.java
@@ -18,22 +18,29 @@ public class Main {
 
             // 숫자 입력
             String input = sc.nextLine();
-            menuNumber = Integer.parseInt(input);
 
-            // 입력 처리
-            if (menuNumber == 0) {
-                System.out.println("프로그램을 종료합니다.");
-                break;
-            } else if (menuNumber == 1) {
-                System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-            } else if (menuNumber == 2) {
-                System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-            } else if (menuNumber == 3) {
-                System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-            } else if (menuNumber == 4) {
-                System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-            } else {
-                System.out.println("잘못된 입력입니다. 0부터 4까지의 숫자를 입력해주세요.");
+            try {
+                // 입력 값 숫자로 변환
+                menuNumber = Integer.parseInt(input);
+
+                // 입력 처리
+                if (menuNumber == 0) {
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                } else if (menuNumber == 1) {
+                    System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+                } else if (menuNumber == 2) {
+                    System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+                } else if (menuNumber == 3) {
+                    System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+                } else if (menuNumber == 4) {
+                    System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+                } else {
+                    System.out.println("잘못된 입력입니다. 0부터 4까지의 숫자를 입력해주세요.");
+                }
+            } catch (NumberFormatException e) {
+                // 숫자가 아닌 입력 처리
+                System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
             }
             System.out.println();
         }
