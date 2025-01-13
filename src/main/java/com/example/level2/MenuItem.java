15688e850f0c6304f02f0ab08cbb431c39137d44
minkyungCHO <mk981015@naver.com>
2025-01-13 15:46:04 +0900

    feat: MenuItem 생성자, Getter, 출력 포멧 구현

--- src/main/java/com/example/level2/MenuItem.java
+++ src/main/java/com/example/level2/MenuItem.java
@@ -1,4 +1,33 @@
 package com.example.level2;
 
 public class MenuItem {
+    private String menuName;
+    private double menuPrice;
+    private String menuDescription;
+
+    // 생성자
+    public MenuItem(String menuName, double menuPrice, String menuDescription) {
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.menuDescription = menuDescription;
+    }
+
+    // Getter
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public double getMenuPrice() {
+        return menuPrice;
+    }
+
+    public String getMenuDescription() {
+        return menuDescription;
+    }
+
+    // 메뉴 출력
+    @Override
+    public String toString() {
+        return String.format("%s   | W %.1f | %s", menuName, menuPrice, menuDescription);
+    }
 }
