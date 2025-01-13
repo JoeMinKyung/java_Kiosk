0c045597fbd185dfd4cd181091f5876786ad2762
minkyungCHO <mk981015@naver.com>
2025-01-13 16:49:20 +0900

    feat: level2의 MenuItem 복사

--- src/main/java/com/example/level3/MenuItem.java
+++ src/main/java/com/example/level3/MenuItem.java
@@ -1,4 +1,37 @@
 package com.example.level3;
 
 public class MenuItem {
+    private static int count = 1; // 메뉴 번호 증가 변수
+    private int menuNumber; // 메뉴 번호
+    private String menuName;
+    private double menuPrice;
+    private String menuDescription;
+
+    // 생성자
+    public MenuItem(String menuName, double menuPrice, String menuDescription) {
+        this.menuNumber = count++;
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.menuDescription = menuDescription;
+    }
+
+    // Getter
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public double getMenuPrice() {
+        return menuPrice;
+    }
+
+    public String getMenuDescription() {
+        return menuDescription;
+    }
+
+    // 메뉴 출력
+    @Override
+    public String toString() {
+        return String.format("%d. %-14s | W %.1f | %s", menuNumber, menuName, menuPrice, menuDescription);
+    }
 }
+
