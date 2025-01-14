903e52eaf6e29388d7a47172f24d1261655aaa38
minkyungCHO <mk981015@naver.com>
2025-01-14 18:51:49 +0900

    feat: toString()만 있던 MenuItem.java에서 toStringWithoutNumber()를 추가하여 숫자 없이 메뉴 출력하는 메서드 분리

--- src/main/java/com/example/level4/MenuItem.java
+++ src/main/java/com/example/level4/MenuItem.java
@@ -8,8 +8,8 @@ public class MenuItem {
     private String menuDescription;
 
     // 생성자
-    public MenuItem(String menuName, double menuPrice, String menuDescription) {
-        this.menuNumber = count++;
+    public MenuItem(int menuNumber, String menuName, double menuPrice, String menuDescription) {
+        this.menuNumber = menuNumber;
         this.menuName = menuName;
         this.menuPrice = menuPrice;
         this.menuDescription = menuDescription;
@@ -32,6 +32,11 @@ public class MenuItem {
     @Override
     public String toString() {
         return String.format("%d. %-14s | W %.1f | %s", menuNumber, menuName, menuPrice, menuDescription);
+    }
+
+    // selectedMenuItem 출력
+    public String toStringWithoutNumber() {
+        return String.format("%s | W %.1f | %s", menuName, menuPrice, menuDescription);
     }
 }
 
