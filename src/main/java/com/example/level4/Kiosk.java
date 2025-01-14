b668919a2de374e64b13b1f92d72a661225b6377
minkyungCHO <mk981015@naver.com>
2025-01-14 18:50:35 +0900

    feat: level4의 Kiosk.java에서 Menu를 분리하여 Kiosk.java에는 사용자 입력 및 처리를 구현함.

--- src/main/java/com/example/level4/Kiosk.java
+++ src/main/java/com/example/level4/Kiosk.java
@@ -6,17 +6,23 @@ import java.util.Scanner;
 
 public class Kiosk {
     private Scanner sc = new Scanner(System.in);
-    private List<MenuItem> menuItems;
+    private List<Menu> menus = new ArrayList<>();
+    ; // 여러 메뉴 카테고리 관리
 
-    // 생성자
-    public Kiosk(List<MenuItem> menuItems) {
-        this.menuItems = menuItems;
+    // 메뉴 카테고리 추가
+    public void addMenu(Menu menu) {
+        menus.add(menu);
     }
 
     // 프로그램 시작 메서드
-    public void start(){
+    public void start() {
         while (true) {
-            printMenu(); // 메뉴 출력
+            // 메인 메뉴 출력
+            System.out.println("[ MAIN MENU ]");
+            for (int i = 0; i < menus.size(); i++) {
+                System.out.printf("%d. %s%n", i + 1, menus.get(i).getCategoryName());
+            }
+            System.out.println("0. 종료      | 종료");
 
             // 숫자 입력
             String input = sc.nextLine();
@@ -32,12 +38,12 @@ public class Kiosk {
                 }
 
                 // 메뉴 번호 유효성 검사
-                if (orderNumber > 0 && orderNumber <= menuItems.size()) {
+                if (orderNumber > 0 && orderNumber <= menus.size()) {
                     // 선택된 메뉴 출력
-                    MenuItem selectedItem = menuItems.get(orderNumber - 1);
-                    System.out.println(selectedItem);
+                    Menu selectedMenu = menus.get(orderNumber - 1);
+                    printMenu(sc, selectedMenu);
                 } else {
-                    System.out.println("잘못된 입력입니다. 0부터 " + menuItems.size() + "까지의 숫자를 입력해주세요.");
+                    System.out.println("잘못된 입력입니다. 0부터 " + menus.size() + "까지의 숫자를 입력해주세요.");
                 }
             } catch (NumberFormatException e) {
                 // 숫자가 아닌 입력 처리
@@ -48,12 +54,38 @@ public class Kiosk {
     }
 
     // 메뉴 출력 메서드
-    private void printMenu() {
-        // 햄버거 메뉴 출력
-        System.out.println("[ SHAKESHACK MENU ]");
-        for (MenuItem menuItem : menuItems) {
-            System.out.println(menuItem);
+    private void printMenu(Scanner sc, Menu menu) {
+        while (true) {
+            System.out.println();
+            // 서브 메뉴 출력
+            System.out.printf("[ %s MENU ]%n", menu.getCategoryName());
+            menu.printMenuItems();
+            System.out.println("0. 뒤로가기");
+
+            // 사용자 입력
+            String input = sc.nextLine();
+
+            try {
+                // 입력 값 숫자로 변환
+                int choice = Integer.parseInt(input);
+
+                // 뒤로 가기
+                if (choice == 0) {
+                    break;
+                }
+
+                // 메뉴 번호 유효성 검사
+                if (choice > 0 && choice <= menu.getMenuItems().size()) {
+                    // 선택된 메뉴 출력
+                    MenuItem selectedItem = menu.getMenuItems().get(choice - 1);
+                    System.out.printf("선택한 메뉴: %s%n", selectedItem.toStringWithoutNumber());
+                } else {
+                    System.out.println("잘못된 입력입니다. 0부터 " + menu.getMenuItems().size() + "까지의 숫자를 입력해주세요.");
+                }
+            } catch (NumberFormatException e) {
+                // 숫자가 아닌 입력 처리
+                System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
+            }
         }
-        System.out.println("0. 종료      | 종료");
     }
 }
