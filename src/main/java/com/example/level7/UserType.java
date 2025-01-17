cfc67cf3daaf00df38f3d3b24e223d4a0aa2e9b0
minkyungCHO <mk981015@naver.com>
2025-01-17 19:47:31 +0900

    feat: 사용자 유형 enum

--- src/main/java/com/example/level7/UserType.java
+++ src/main/java/com/example/level7/UserType.java
@@ -1,5 +1,18 @@
 package com.example.level7;
 
 public enum UserType {
+    VETERAN(0.10), // 국가유공자: 10%
+    SOLDIER(0.05), // 군인: 5%
+    STUDENT(0.03), // 학생: 3%
+    GENERAL(0.0);  // 일반: 0%
 
+    private final double discountRate;
+
+    UserType(double discountRate) {
+        this.discountRate = discountRate;
+    }
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
 }
