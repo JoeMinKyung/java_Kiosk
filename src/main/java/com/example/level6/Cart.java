e026f2e7ada198a15721fd1102bb18e2d8bbd724
minkyungCHO <mk981015@naver.com>
2025-01-17 15:22:53 +0900

    feat: 장바구니 항목 추가

--- src/main/java/com/example/level6/Cart.java
+++ src/main/java/com/example/level6/Cart.java
@@ -6,4 +6,10 @@ import java.util.List;
 public class Cart {
     private List<MenuItem> items = new ArrayList<>();
 
+    // 장바구니에 항목 추가
+    public void addItem(MenuItem item) {
+        items.add(item);
+    }
+
+
 }
